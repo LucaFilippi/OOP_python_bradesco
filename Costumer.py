fce1e3a01f0284da2fa22899e5e882649bf56a25
Luca Francesco Filippi <luca.ffilippi@gmail.com>
2025-06-16 07:40:17 -0300

    get and set method

--- Costumer.py
+++ Costumer.py
@@ -1,4 +1,13 @@
 class Costumer:
         def __init__(self, n, phone):
-                self.name = n
-                self.number = phone
+                self.__name = n
+                self._number = phone
+        
+        #get method
+        def get_name(self):
+                return self.__name
+        
+        #set method
+        def set_name(self, name):
+                self.name = name
+
--- Main.py
+++ Main.py
@@ -6,8 +6,8 @@ from Costumer import Costumer
 from Account import Account
 
 c1 = Costumer("Peter", "555-0878")
-account = Account("665", c1.name, 0)
+account = Account("665", c1.__name, 0)
 
 print(c1)
-print(f"name is {c1.name} and phone number is {c1.number}")
-print(f"{account.owner}, number: {account.number}, current balance: {account.balance}")
+print(f"name is {c1.__name} and phone number is {c1._number}")
+print(f"{account.owner}, number: {account.number}, current balance: {account.__balance}")
